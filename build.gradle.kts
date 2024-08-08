ca6f71105b5078d440136d2a24f58a51070f56af
claudioaltamura <claudio@claudioaltamura.de>
2024-08-08 16:55:10 +0200

    build: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.0-M1")
-    testImplementation("org.assertj:assertj-core:3.25.3")
+    testImplementation("org.assertj:assertj-core:3.26.3")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.0-M1")
 }
